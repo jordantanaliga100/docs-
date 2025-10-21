8ec22e7b4321333196bb2abe372496f3b9fb6f9d
jordantanaliga100 <jordantanaliga100@gmail.com>
2025-10-21 15:54:02 +0800

    chore(setups): changed repo | added ignore files

---
+++ Readme.md
@@ -0,0 +1,19 @@
+1. Simple & Clear:
+
+📘 A collection of REST API documentation using Swagger for clear, structured, and interactive API references.
+
+2. Developer-Focused:
+
+Comprehensive REST API documentation built with Swagger — designed for developers to easily explore and understand API endpoints.
+
+3. Professional & Clean:
+
+Centralized API documentation hub powered by Swagger. Provides interactive and up-to-date references for RESTful services.
+
+4. Short GitHub-style Description:
+
+REST API documentation powered by Swagger UI and OpenAPI specs.
+
+5. Descriptive + Purpose-driven:
+
+A documentation repository showcasing how to describe, structure, and present REST APIs using Swagger and OpenAPI standards.
---
+++ swaggerUi/src/app.ts
@@ -0,0 +1,69 @@
+// IMPORTS
+import cors from "cors";
+import dotenv from "dotenv";
+
+import express, { Express, Request, Response } from "express";
+// import morgan from "morgan";
+import path from "path";
+import UserRoutes from "./app/users/user.route";
+
+// ENV CONFIG
+const nodeEnv = process.env.NODE_ENV || "development";
+const envFile = nodeEnv === "production" ? ".env.prod" : ".env.local";
+dotenv.config({ path: envFile });
+console.log(`✅ Loaded envFile`);
+console.log("... on " + nodeEnv + " environment 🚀");
+
+const app = express() as Express;
+
+// TOP MIDDLEWARES
+app.set("trust proxy", 1);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.join(__filename);
+app.use(express.static(path.resolve(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
+// app.use(morgan("dev"));
+
+// ROUTES
+// app.get("/", (req: Request, res: Response) => {
+//   // throw new Error("Testing gin index");
+//   res.send(`
+//     <small>
+//     <h3>
+//     Node_Express Server Alive 🛩️
+//     </h3>
+//     <a href="/api-docs">API </a>
+//     </small>
+//     `);
+// });
+// ✅ SWAGGER DOCS
+// app.get("/api-docs", (req: Request, res: Response) => {
+//   res.send(`
+//     <a href="/"> Go back</a>
+//     <pre>
+//     API Docs here...
+//     </pre>
+//     `);
+// });
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  const swaggerUiPath = path.resolve(__dirname, "../public/swagger.html");
+  res.sendFile(swaggerUiPath);
+});
+
+app.use("/api/v1/users", UserRoutes);
+
+export default app;
